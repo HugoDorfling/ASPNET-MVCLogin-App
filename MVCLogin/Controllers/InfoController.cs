47511e9481729ff8fa5a58c720e11e3524f005c7
Hugo Dorfling <hugodorfling@live.co.za>
2020-08-07 15:50:03 +0200

    add jquery + popper + bootstrap, implement modal popup for editing and storing updated info

--- MVCLogin/Controllers/InfoController.cs
+++ MVCLogin/Controllers/InfoController.cs
@@ -40,5 +40,50 @@ namespace MVCLogin.Controllers
             // Send List of InfoVM (ViewModel) to view 
             return View(InfoVMList);
         }
+
+        public ActionResult GetInfoPartial()
+        {
+            LoginDatabaseEntities1 infodb = new LoginDatabaseEntities1();
+
+            InfoVM InfoVMList = new InfoVM();
+            var info = (from Pers in infodb.People
+                            join Inf in infodb.Infoes on Pers.ID equals Inf.PersonId
+                            select new
+                            { Pers.Name, Pers.Surname, Inf.PersonId, Inf.TelNo, Inf.CellNo, Inf.AddressLine1, Inf.AddressLine2, Inf.AddressLine3, Inf.AddressCode, Inf.PostalAddress1, Inf.PostalAddress2, Inf.PostalCode });
+
+            foreach (var item in info)
+            {
+                InfoVM objivm = new InfoVM(); // ViewModel
+                objivm.PersonId = item.PersonId;
+                objivm.Name = item.Name;
+                objivm.Surname = item.Surname;
+                objivm.TelNo = item.TelNo;
+                objivm.CellNo = item.CellNo;
+                objivm.AddressLine1 = item.AddressLine1;
+                objivm.AddressLine2 = item.AddressLine2;
+                objivm.AddressLine3 = item.AddressLine3;
+                objivm.AddressCode = item.AddressCode;
+                objivm.PostalAddress1 = item.PostalAddress1;
+                objivm.PostalAddress2 = item.PostalAddress2;
+                objivm.PostalCode = item.PostalCode;
+                return PartialView("_CreateOrUpdateInfoPartial", objivm);
+            }
+
+            return PartialView("");
+        }
+
+        public ActionResult CreateOrUpdateInfo(Info info)
+        {
+
+            if (ModelState.IsValid)
+            {
+                LoginDatabaseEntities1 infodb = new LoginDatabaseEntities1();
+                infodb.Infoes.Add(info);
+                infodb.SaveChanges();
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
--- MVCLogin/ViewModels/InfoVM.cs
+++ MVCLogin/ViewModels/InfoVM.cs
@@ -7,6 +7,7 @@ namespace MVCLogin.ViewModels
 {
     public class InfoVM
     {
+        public int PersonId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Password { get; set; }
